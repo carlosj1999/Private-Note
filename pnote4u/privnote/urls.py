7089a391c4545011b98ee6b15dd0affc8c211812
Carlos J Ibanez <77297452+carlosj1999@users.noreply.github.com>
2024-10-12 13:28:07 -0400

    Delete pnote4u directory

--- pnote4u/privnote/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import create_note_view, note_created_view, view_note_view
-
-urlpatterns = [
-    path('', create_note_view, name='create_note'),
-    path('created/<uuid:unique_link>/', note_created_view, name='note_created'),
-    path('<uuid:unique_link>/', view_note_view, name='view_note'),
-]
